2404adc2076b2169fcbbb310365934d374096539
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-02 16:16:55 +0800

    :rocket:

--- main.go
+++ main.go
@@ -1,26 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	// Create File
-	f, err := os.Create("sample.enc")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	// Writing File
-	_, err = f.WriteString("absdlkfj \n")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("done")
 }
