c69a3866352be215d6c49fa65ab43f4cb5dae397
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-03 13:49:19 +0800

    :rocket:

--- event/read.go
+++ event/read.go
@@ -9,14 +9,15 @@ func Read(path string) (string, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		log.Println(err)
-		return nil, err
+		return "error", err
 	}
 	data := []byte{}
 	length, err := f.Read(data)
 	if err != nil {
 		log.Println(err)
-		return nil, err
+		return "error", err
 	}
 	val := string(data[:length])
+	f.Close()
 	return val, nil
 }
