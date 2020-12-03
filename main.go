2a8edd4c6e08bc2c93abad26a00883cf692e915a
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-03 11:24:03 +0800

    :rocket:

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"encrypt/event"
+	"fmt"
+	"log"
+)
+
 func main() {
+	f, err := event.Create("hello")
+	if err != nil {
+		log.Println(err)
+	}
+
+	val, err := event.Write(f, "aaa")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(val)
 }
