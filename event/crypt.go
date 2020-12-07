269bf5c0e9db4277f4597f45b30abe710516e406
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-07 10:24:26 +0800

    :rocket:

--- event/crypt.go
+++ event/crypt.go
@@ -17,7 +17,7 @@ func createHash(key string) string {
 }
 
 // Encrypt data with our passphrase
-func encrypt(data []byte, passphrase string) []byte {
+func Encrypt(data []byte, passphrase string) []byte {
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
@@ -32,7 +32,7 @@ func encrypt(data []byte, passphrase string) []byte {
 }
 
 // Decrypt data with our passphrase
-func decrypt(data []byte, passphrase string) []byte {
+func Decrypt(data []byte, passphrase string) []byte {
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
