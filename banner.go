f7fc71a542a09c4332366b08205e41ad455338ae
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-08 13:38:34 +0800

    :rocket:

--- banner.go
+++ banner.go
@@ -1,17 +1,17 @@
 package encrypt
 
-import "fmt"
+import "encrypt/colors"
 
 func banner() {
-	fmt.Println("8888888888                                             888    ")
-	fmt.Println("888                                                    888    ")
-	fmt.Println("888                                                    888    ")
-	fmt.Println("8888888    88888b.   .d8888b 888d888 888  888 88888b.  888888 ")
-	fmt.Println("888        888  88b d88P     888P    888  888 888  88b 888    ")
-	fmt.Println("888        888  888 888      888     888  888 888  888 888    ")
-	fmt.Println("888        888  888 Y88b.    888     Y88b 888 888 d88P Y88b.  ")
-	fmt.Println("8888888888 888  888   Y8888P 888       Y88888 88888P     Y888 ")
-	fmt.Println("                                          888 888             ")
-	fmt.Println("                                     Y8b d88P 888             ")
-	fmt.Println("                                       Y88P   888             ")
+	colors.Blue("GGGGGGGGGG                                             GGG    ")
+	colors.Blue("GGG                                                    GGG    ")
+	colors.Blue("GGG                                                    GGG    ")
+	colors.Blue("GGGGGGG    GGGGGb.   .dGGGGb GGGdGGG GGG  GGG GGGGGb.  GGGGGG ")
+	colors.Blue("GGG        GGG  GGb dGGP     GGGP    GGG  GGG GGG  GGb GGG    ")
+	colors.Blue("GGG        GGG  GGG GGG      GGG     GGG  GGG GGG  GGG GGG    ")
+	colors.Blue("GGG        GGG  GGG YGGb.    GGG     YGGb GGG GGG dGGP YGGb.  ")
+	colors.Blue("GGGGGGGGGG GGG  GGG   YGGGGP GGG       YGGGGG GGGGGP     YGGG ")
+	colors.Blue("                                          GGG GGG             ")
+	colors.Blue("                                     YGb dGGP GGG             ")
+	colors.Blue("                                       YGGP   GGG             ")
 }
