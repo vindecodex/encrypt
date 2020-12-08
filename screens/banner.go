2e6611546bbef4e89eb8c0ec2800dbe5c863a451
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-08 14:09:49 +0800

    :rocket:

---
+++ screens/banner.go
@@ -0,0 +1,17 @@
+package screens
+
+import "encrypt/colors"
+
+func Banner() {
+	colors.SetColor("GGGGGGGGGG                                             GGG    ", colors.Blue)
+	colors.SetColor("GGG                                                    GGG    ", colors.Blue)
+	colors.SetColor("GGG                                                    GGG    ", colors.Blue)
+	colors.SetColor("GGGGGGG    GGGGGb.   .dGGGGb GGGdGGG GGG  GGG GGGGGb.  GGGGGG ", colors.Blue)
+	colors.SetColor("GGG        GGG  GGb dGGP     GGGP    GGG  GGG GGG  GGb GGG    ", colors.Blue)
+	colors.SetColor("GGG        GGG  GGG GGG      GGG     GGG  GGG GGG  GGG GGG    ", colors.Blue)
+	colors.SetColor("GGG        GGG  GGG YGGb.    GGG     YGGb GGG GGG dGGP YGGb.  ", colors.Blue)
+	colors.SetColor("GGGGGGGGGG GGG  GGG   YGGGGP GGG       YGGGGG GGGGGP     YGGG ", colors.Blue)
+	colors.SetColor("                                          GGG GGG             ", colors.Blue)
+	colors.SetColor("                                     YGb dGGP GGG             ", colors.Blue)
+	colors.SetColor("                                       YGGP   GGG             ", colors.Blue)
+}
