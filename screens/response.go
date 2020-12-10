9bf58454418a018cfe9cf97089e223a919e760ba
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-10 14:42:07 +0800

    :rocket:

--- screens/response.go
+++ screens/response.go
@@ -2,10 +2,12 @@ package screens
 
 import (
 	"encrypt/colors"
+	"fmt"
 )
 
 func Input(input interface{}) {
 	colors.SetColor("encrypt: ", colors.Blue)
+	fmt.Scanln(input)
 }
 func Response(res string) {
 	colors.SetColorLn(res, colors.Blue)
