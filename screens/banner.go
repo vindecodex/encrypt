1430afb88e89fa54ea82fb0d8f9d22daa57b6abd
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-10 14:09:05 +0800

    :art:

--- screens/banner.go
+++ screens/banner.go
@@ -3,15 +3,15 @@ package screens
 import "encrypt/colors"
 
 func Banner() {
-	colors.SetColor("GGGGGGGGGG                                             GGG    ", colors.Blue)
-	colors.SetColor("GGG                                                    GGG    ", colors.Blue)
-	colors.SetColor("GGG                                                    GGG    ", colors.Blue)
-	colors.SetColor("GGGGGGG    GGGGGb.   .dGGGGb GGGdGGG GGG  GGG GGGGGb.  GGGGGG ", colors.Blue)
-	colors.SetColor("GGG        GGG  GGb dGGP     GGGP    GGG  GGG GGG  GGb GGG    ", colors.Blue)
-	colors.SetColor("GGG        GGG  GGG GGG      GGG     GGG  GGG GGG  GGG GGG    ", colors.Blue)
-	colors.SetColor("GGG        GGG  GGG YGGb.    GGG     YGGb GGG GGG dGGP YGGb.  ", colors.Blue)
-	colors.SetColor("GGGGGGGGGG GGG  GGG   YGGGGP GGG       YGGGGG GGGGGP     YGGG ", colors.Blue)
-	colors.SetColor("                                          GGG GGG             ", colors.Blue)
-	colors.SetColor("                                     YGb dGGP GGG             ", colors.Blue)
-	colors.SetColor("                                       YGGP   GGG             ", colors.Blue)
+	colors.SetColorLn("GGGGGGGGGG                                             GGG    ", colors.Blue)
+	colors.SetColorLn("GGG                                                    GGG    ", colors.Blue)
+	colors.SetColorLn("GGG                                                    GGG    ", colors.Blue)
+	colors.SetColorLn("GGGGGGG    GGGGGb.   .dGGGGb GGGdGGG GGG  GGG GGGGGb.  GGGGGG ", colors.Blue)
+	colors.SetColorLn("GGG        GGG  GGb dGGP     GGGP    GGG  GGG GGG  GGb GGG    ", colors.Blue)
+	colors.SetColorLn("GGG        GGG  GGG GGG      GGG     GGG  GGG GGG  GGG GGG    ", colors.Blue)
+	colors.SetColorLn("GGG        GGG  GGG YGGb.    GGG     YGGb GGG GGG dGGP YGGb.  ", colors.Blue)
+	colors.SetColorLn("GGGGGGGGGG GGG  GGG   YGGGGP GGG       YGGGGG GGGGGP     YGGG ", colors.Blue)
+	colors.SetColorLn("                                          GGG GGG             ", colors.Blue)
+	colors.SetColorLn("                                     YGb dGGP GGG             ", colors.Blue)
+	colors.SetColorLn("                                       YGGP   GGG             ", colors.Blue)
 }
