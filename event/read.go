4d6827e8db4fd41c196ce027151ac23dc8290136
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-14 13:09:06 +0800

    :rocket:

--- event/read.go
+++ event/read.go
@@ -1,15 +1,14 @@
 package event
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 )
 
 func Read(filePath string, passphrase string) (string, error) {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		fmt.Println("@@@@@@@@@@@")
-		// log.Println(err)
+		log.Println(err)
 		return "error:12", err
 	}
 
