e63cbaaf6f582f144ea6b4c3e7bda611804ffbcc
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-17 13:34:53 +0800

    :rocket:

--- screens/help.go
+++ screens/help.go
@@ -1,3 +1,13 @@
 package screens
 
-var Help map[string]string
+import "encrypt/colors"
+
+const Help = map[string]string{
+	"-help": "[ Gives available commands ]",
+}
+
+func ShowCommands() {
+	for k, v := range Help {
+		colors.SetColorln(k, " > ", v)
+	}
+}
