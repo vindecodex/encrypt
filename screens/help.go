ade8ee5f658e31de2994d5296e99da427f021e7b
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-17 13:43:32 +0800

    remove type CMD

--- screens/help.go
+++ screens/help.go
@@ -2,15 +2,13 @@ package screens
 
 import "encrypt/colors"
 
-type CMD string
-
-var Help = map[CMD]string{
+var Help = map[string]string{
 	"-help": "[ Gives available commands ]",
 }
 
 func ShowCommands() {
 	for k, v := range Help {
-		text := string(k + " > " + v)
+		text := k + " > " + v
 		colors.SetColorln(text, colors.White)
 	}
 }
