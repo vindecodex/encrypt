4b167057bbc1b791532d1bf6456f98779c6474f5
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-18 15:35:30 +0800

    :rocket: test

--- event/listener.go
+++ event/listener.go
@@ -1,25 +1,26 @@
 package event
 
-import "fmt"
+import (
+	"encrypt/colors"
+	"encrypt/handler"
+	"encrypt/screens"
+)
 
 func Listen(cmd *string) bool {
-
-	fmt.Println(*cmd)
-	return true
-	// switch cmd {
-	// case "help":
-	// colors.SetColorln("Available Commands: ", colors.White)
-	// screens.ShowCommands()
-	// return true
-	// case "clear":
-	// screens.Clear()
-	// return true
-	// case "open":
-	// handler.OpenFile()
-	// return true
-	// default:
-	// colors.SetColor("Unknown Command", colors.Red)
-	// colors.SetColorln(": help [ for helpful commands ]", colors.White)
-	// return true
-	// }
+	switch *cmd {
+	case "help":
+		colors.SetColorln("Available Commands: ", colors.White)
+		screens.ShowCommands()
+		return true
+	case "clear":
+		screens.Clear()
+		return true
+	case "open":
+		handler.OpenFile()
+		return true
+	default:
+		colors.SetColor("Unknown Command", colors.Red)
+		colors.SetColorln(": help [ for helpful commands ]", colors.White)
+		return true
+	}
 }
