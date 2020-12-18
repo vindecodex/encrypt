e63fc1054b2edebb8596cbdabaf2f1e530f806b7
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-18 14:38:40 +0800

    :rocket: change orders

--- event/listener.go
+++ event/listener.go
@@ -7,12 +7,12 @@ import (
 
 func Listen(cmd string) bool {
 	switch cmd {
-	case "clear":
-		screens.Clear()
-		return true
 	case "help":
 		colors.SetColorln("Available Commands: ", colors.White)
 		screens.ShowCommands()
+		return true
+	case "clear":
+		screens.Clear()
 		return true
 	default:
 		colors.SetColorln("Unknown Command: help [ for helpful commands ]", colors.White)
