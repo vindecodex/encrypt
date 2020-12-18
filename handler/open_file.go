0541bfd8047376796dcde9a383bc5ef9f0396573
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-18 15:46:32 +0800

    handling file opening

--- handler/open_file.go
+++ handler/open_file.go
@@ -2,11 +2,22 @@ package handler
 
 import (
 	"encrypt/colors"
+	"encrypt/event"
 	"fmt"
+	"log"
 )
 
-func OpenFile(cmd *string) {
+func OpenFile(cmd *string) (string, error) {
 	colors.SetColor("File: ", colors.Blue)
 	fmt.Scanln(cmd)
-	fmt.Println(*cmd)
+	file := *cmd
+	colors.SetColor("Passphrase: ", colors.Blue)
+	passphrase := *cmd
+
+	val, err := event.Read(file, passphrase)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	return val, nil
 }
