b7425d0675cfbf14769b278bd7d985531a71b7af
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-18 14:35:54 +0800

    :x: hide help map

--- screens/help.go
+++ screens/help.go
@@ -2,14 +2,14 @@ package screens
 
 import "encrypt/colors"
 
-var Help = map[string]string{
+var help = map[string]string{
 	"help":  "[ Gives available commands ]",
 	"close": "[ End encrypt process ]",
 	"clear": "[ Clear the terminal ]",
 }
 
 func ShowCommands() {
-	for k, v := range Help {
+	for k, v := range help {
 		text := k + " > " + v
 		colors.SetColorln(text, colors.White)
 	}
